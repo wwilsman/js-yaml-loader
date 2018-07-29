3593bf0ee0ff714746288de730ce4984d674e5d3
error <error@error-unlimited.net>
2018-07-29 18:26:10 -0500

    add safe option

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { inspect } = require('util');
+const { getOptions } = require('loader-utils');
 const yaml = require('js-yaml');
 
 module.exports = function(source) {
   this.cacheable && this.cacheable();
 
   try {
-    const res = yaml.safeLoad(source);
+    const options = getOptions(this);
+    const safe = options.safe !== false;
+    const res = safe ? yaml.safeLoad(source) : yaml.load(source);
     return `module.exports = ${inspect(res, { depth: null })}`;
   } catch (err) {
     this.emitError(err);
